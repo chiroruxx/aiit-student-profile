271e49d51f07b814360a82de016d095bffdcd22b
mi-bear <kumajet@gmail.com>
2021-12-27 18:46:47 +0900

    fixed by rubocop --auto-correct

--- db/schema.rb
+++ db/schema.rb
@@ -9,9 +9,9 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
+# frozen_string_literal: true
 
-ActiveRecord::Schema.define(version: 2021_12_27_081909) do
-
+ActiveRecord::Schema.define(version: 20_211_227_081_909) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
