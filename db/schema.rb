829d6565733c82fa224fdd836fdfdb2acbc20d18
mi-bear <kumajet@gmail.com>
2021-12-27 18:28:25 +0900

    fixed converted to single-quoted strings

--- db/schema.rb
+++ db/schema.rb
@@ -13,32 +13,32 @@
 ActiveRecord::Schema.define(version: 2021_12_27_081909) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "user_profiles", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "name", null: false
-    t.string "name_kana", null: false
-    t.string "nickname", null: false
-    t.string "email", null: false
-    t.string "major_subject", null: false
-    t.string "started", null: false
-    t.text "work"
-    t.text "background"
-    t.text "hobby"
-    t.string "pbl"
-    t.string "favorite_food"
-    t.string "hated_food"
-    t.text "other"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_user_profiles_on_user_id"
+  create_table 'user_profiles', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.string 'name', null: false
+    t.string 'name_kana', null: false
+    t.string 'nickname', null: false
+    t.string 'email', null: false
+    t.string 'major_subject', null: false
+    t.string 'started', null: false
+    t.text 'work'
+    t.text 'background'
+    t.text 'hobby'
+    t.string 'pbl'
+    t.string 'favorite_food'
+    t.string 'hated_food'
+    t.text 'other'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['user_id'], name: 'index_user_profiles_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'users', force: :cascade do |t|
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  add_foreign_key "user_profiles", "users"
+  add_foreign_key 'user_profiles', 'users'
 end
