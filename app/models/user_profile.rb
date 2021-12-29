49129b5fb8df53df88fd4926e5488dcc4d398d47
Martin <polyu17002407g@gmail.com>
2021-12-29 17:15:41 +0900

    受講した科目、検証などのコードを更新する

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,19 +1,17 @@
 # frozen_string_literal: true
 
 class UserProfile < ApplicationRecord
-  belongs_to :user, foreign_key: 'user_id', dependent: :destroy
+  belongs_to :user, dependent: :destroy
+  has_many :user_profile_subjects, dependent: :destroy
+  has_many :subjects, through: :user_profile_subjects
 
   validates :name, presence: true, length: { maximum: 128 }
-  validates :name_kana, presence: true, length: { maximum: 128 }, allow_nil: true
-  validates :nickname, presence: true, length: { maximum: 128 }, allow_nil: true
+  validates :name_kana, length: { maximum: 128 }, allow_nil: true
+  validates :nickname, length: { maximum: 128 }, allow_nil: true
   validates :email, presence: true, length: { maximum: 255 }
   validates :major_subject, presence: true, length: { maximum: 255 }
-  validates :started, presence: true, length: { maximum: 4 }
-  validates :work, presence: true, allow_nil: true
-  validates :background, presence: true, allow_nil: true
-  validates :hobby, presence: true, allow_nil: true
-  validates :pbl, presence: true, length: { maximum: 128 }, allow_nil: true
-  validates :favorite_food, presence: true, length: { maximum: 128 }, allow_nil: true
-  validates :hated_food, presence: true, length: { maximum: 128 }, allow_nil: true
-  validates :other, presence: true, allow_nil: true
+  validates :started, numericality: { only_integer: true, greater_than_or_equal_to: Date.current.year - 10 , less_than_or_equal_to: Date.current.year + 10 }
+  validates :pbl, length: { maximum: 128 }, allow_nil: true
+  validates :favorite_food, length: { maximum: 128 }, allow_nil: true
+  validates :hated_food, length: { maximum: 128 }, allow_nil: true
 end
