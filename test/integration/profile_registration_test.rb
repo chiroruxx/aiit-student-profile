d1fbf7404cb5dd713880d1e49dab1cb70a97dd0f
Martin <polyu17002407g@gmail.com>
2021-12-30 01:33:32 +0900

    init ProfileRegistrationTest

---
+++ test/integration/profile_registration_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class ProfileRegistrationTest < ActionDispatch::IntegrationTest
+  def setup
+    @user_profile = user_profiles(:one)
+  end
+  # test "the truth" do
+  #   assert true
+  # end
+end
