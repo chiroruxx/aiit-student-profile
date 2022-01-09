a3ae7c382e42214672b2b8bf200a9cd86098ffca
mi-bear <kumajet@gmail.com>
2022-01-09 14:37:06 +0900

    added email to users and fixtures

--- db/migrate/20211227081801_create_users.rb
+++ db/migrate/20211227081801_create_users.rb
@@ -6,7 +6,8 @@ class CreateUsers < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       t.string :name, null: false, limit: 128
       t.string :provider, null: false, limit: 128
-      t.string :uid, null: false, limit: 16
+      t.string :uid, null: false, limit: 36
+      t.string :email, null: false, limit: 255
       t.string :image
       t.timestamps
     end
