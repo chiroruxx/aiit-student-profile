7e1eaec18a4d56690dd19c881f992344d5e973c0
chiroruxx <dormir.kazu@gmail.com>
2022-01-14 18:53:09 +0900

    エラーページ作成

---
+++ app/controllers/errors_controller.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# ErrorsController
+class ErrorsController < ApplicationController
+  layout 'full'
+
+  def show
+    status_code = params[:code] || 500
+    @message = message(status_code)
+
+    render 'default', status: status_code
+  end
+
+  private
+
+  def message(code)
+    case code.to_i
+    when 400
+      '不正なリクエストです。'
+    when 404
+      'ページが見つかりません。'
+    else
+      'サーバエラーが発生しました。'
+    end
+  end
+end
