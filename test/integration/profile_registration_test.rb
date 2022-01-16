95c0f26152613ca617ca0d6af7060042f3ef090a
Martin <polyu17002407g@gmail.com>
2022-01-16 12:34:12 +0900

    fix test cases

--- test/integration/profile_registration_test.rb
+++ test/integration/profile_registration_test.rb
@@ -5,52 +5,52 @@ require 'test_helper'
 class ProfileRegistrationTest < ActionDispatch::IntegrationTest
   setup do
     OmniAuth.config.test_mode = true
-    sign_in
-
-    @user = users(:three)
   end
 
   teardown do
     OmniAuth.config.test_mode = false
   end
 
+  test 'should redirect register when not signed in' do
+    sign_out
+    get register_path
+    assert_not signed_in?
+    assert_redirected_to root_path
+  end
+
   test 'unsuccessful registration' do
+    sign_in
     # login and do not have user_profile
-    if signed_in?
-      get register_path
-      assert_template 'user_profiles/new'
-      assert_no_difference 'UserProfile.count' do
-        post user_profiles_path, params: {
-          user_profile: {
-            user_id: 3,
-            name: '',
-            email: '',
-            major_subject: '',
-            started: ''
-          }
+    get register_path
+    assert_template 'user_profiles/new'
+    assert_no_difference 'UserProfile.count' do
+      post user_profiles_path, params: {
+        user_profile: {
+          name: '',
+          email: '',
+          major_subject: '',
+          started: ''
         }
-      end
-      assert_template 'user_profiles/new'
+      }
     end
+    assert_template 'user_profiles/new'
   end
 
   test 'successful registration' do
+    sign_in
     # login and do not have user_profile
-    if signed_in?
-      get register_path
-      assert_template 'user_profiles/new'
-      assert_difference 'UserProfile.count', 1 do
-        post user_profiles_path, params: {
-          user_profile: {
-            user_id: 3,
-            name: 'man',
-            email: 'man@man.com',
-            major_subject: '情報アーキテクチャ',
-            started: '2021'
-          }
+    get register_path
+    assert_template 'user_profiles/new'
+    assert_difference 'UserProfile.count', 1 do
+      post user_profiles_path, params: {
+        user_profile: {
+          name: 'mockuser',
+          email: 'mock@example.com',
+          major_subject: '情報アーキテクチャ',
+          started: '2021'
         }
-      end
-      assert_redirected_to user_profiles_path
+      }
     end
+    assert_redirected_to user_profiles_path
   end
 end
