afc1034385f05aee466ec85afc80cf7b4deb9b3f
Masanori Ebizuka <ebisennet@gmail.com>
2022-01-25 19:53:21 +0900

    Add test code of error pages.

--- test/integration/errors_show_test.rb
+++ test/integration/errors_show_test.rb
@@ -3,5 +3,7 @@ require "test_helper"
 class ErrorsShowTest < ActionDispatch::IntegrationTest
   test 'show error page' do
     get '/404'
+    assert_equal 404, status
+    assert_template 'errors/show'
   end
 end
