b889df9d33260b741cd3874e4d099872235f57c9
Masanori Ebizuka <ebisennet@gmail.com>
2022-01-25 19:54:15 +0900

    Test setting of error pages.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -25,8 +25,8 @@ Rails.application.configure do
   }
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
   config.cache_store = :null_store
 
   # Raise exceptions instead of rendering exception templates.
